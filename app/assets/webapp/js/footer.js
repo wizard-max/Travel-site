04e25605790375a8d3f1db77c5a332072825188a
mahsadinar <mahsa.dinarvand2018@gmail.com>
2020-04-11 20:02:57 +0200

    add

---
+++ app/assets/webapp/js/footer.js
@@ -0,0 +1,122 @@
+footer="<div class='footer-bottom'>" +
+        "<div class='container'>" +
+			"<!-- Grid row-->" +
+            "<div class='row py-4 d-flex align-items-center'>" +
+
+                "<!-- Grid column -->" +
+                "<div class='col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0'>" +
+                    "<h6 class='mb-0'>Get connected with us on social networks!</h6>" +
+                "</div>" +
+                "<!-- Grid column -->" +
+                "<div class='col-md-6 col-lg-7 text-center text-md-right'>" +
+
+                    "<!-- Facebook -->" +
+                    "<a class='fb-ic'>" +
+                        "<i class='fab fa-facebook-f white-text mr-4'> </i>" +
+                    "</a>" +
+                    "<!-- Twitter -->" +
+                    "<a class='tw-ic'>" +
+                        "<i class='fab fa-twitter white-text mr-4'> </i>" +
+                    "</a>" +
+                    "<!-- Google +-->" +
+                    "<a class='gplus-ic'>" +
+                        "<i class='fab fa-google-plus-g white-text mr-4'> </i>" +
+                    "</a>" +
+                    "<!--Linkedin -->" +
+                    "<a class='li-ic'>" +
+                        "<i class='fab fa-linkedin-in white-text mr-4'> </i>" +
+                    "</a>" +
+                    "<!--Instagram-->" +
+                    "<a class='ins-ic'>" +
+                        "<i class='fab fa-instagram white-text'> </i>" +
+                    "</a>" +
+
+                "</div>" +
+                "<!-- Grid column -->" +
+
+            "</div>" +
+            "<!-- Grid row-->" +
+
+        "</div>" +
+    "</div>" +
+
+    "<!-- Footer Links -->" +
+    "<div class='container text-center text-md-left mt-5'>" +
+
+        "<!-- Grid row -->" +
+        "<div class='row mt-3'>" +
+
+            "<!-- Grid column -->" +
+            "<div class='col-md-3 col-lg-4 col-xl-3 mx-auto mb-4'>" +
+
+                "<!-- Content -->" +
+                "<h6 class='text-uppercase font-weight-bold'>VROOM Automotive</h6>" +
+                "<hr class='deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto'>" +
+                "<p>when you choose VROOM Automotive, Contact us today to learn more about our services.</p>" +
+
+            "</div>" +
+            "<!-- Grid column -->" +
+
+            "<!-- Grid column -->" +
+            "<div class='col-md-2 col-lg-2 col-xl-2 mx-auto mb-4'>" +
+
+                "<!-- Links -->" +
+                "<h6 class='text-uppercase font-weight-bold'> CarServices</h6>" +
+                "<hr class='deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto'>" +
+                "<p>" +
+                    "<a href='#!'>BMW</a>" +
+                "</p>" +
+                "<p>" +
+                    "<a href='#!'>Porsche</a>" +
+                "</p>" +
+            "</div>" +
+            "<!-- Grid column -->" +
+
+            "<!-- Grid column -->" +
+            "<div class='col-md-3 col-lg-2 col-xl-2 mx-auto mb-4'>" +
+
+                "<!-- Links -->" +
+                "<h6 class='text-uppercase font-weight-bold'>Useful links</h6>" +
+                "<hr class='deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto'>" +
+                "<p>" +
+                    "<a href='#!'>Your Account</a>" +
+                "</p>" +
+
+                "<p>" +
+                    "<a href='#!'>Shipping Rates</a>" +
+                "</p>" +
+                "<p>" +
+                    "<a href='#!'>Help</a>" +
+                "</p>" +
+
+            "</div>" +
+            "<!-- Grid column -->" +
+
+            "<!-- Grid column -->" +
+            "<div class='col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4'>" +
+
+                "<!-- Links -->" +
+                "<h6 class='text-uppercase font-weight-bold'>Contact</h6>" +
+                "<hr class='deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto'>" +
+                "<p>" +
+                    "<i class='fas fa-home mr-3'></i> Via Gradenigo 6, Padova (PD), Italy</p>" +
+                "<p>" +
+                    "<i class='fas fa-envelope mr-3'></i> info@example.com</p>" +
+                "<p>" +
+                    "<i class='fas fa-phone mr-3'></i> + 039 234 567 88</p>" +
+                "<p>" +
+                    "<i class='fas fa-print mr-3'></i> + 039 234 567 89</p>" +
+
+            "</div>" +
+            "<!-- Grid column -->" +
+
+        "</div>" +
+        "<!-- Grid row -->" +
+
+    "</div>" +
+    "<!-- Footer Links -->" +
+
+    "<!-- Copyright -->" +
+    "<div class='footer-copyright text-center py-3'>© 2020 Copyright:" +
+        "<a href='https://mdbootstrap.com/'> MDBootstrap.com</a>" +
+    "</div> <!-- Copyright -->";
